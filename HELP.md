1d48bdcbf8d5ee26f04b92a11863fbda700ddba3
Hai Liang Wang <hai@chatopera.com>
2023-03-30 10:02:37 +0800

    Update files

---
+++ HELP.md
@@ -0,0 +1,19 @@
+# Blogs sharing Thoughts and Learnings
+
+[https://hailiang-wang.github.io/](https://hailiang-wang.github.io/)
+
+## Deps
+
+Pandoc, https://pandoc.org/
+
+## Add and update
+
+Put files into docs, add entry into [docs/README.md](docs/README.md).
+Next, run `./scripts/commit.sh`.
+
+Later [site](https://hailiang-wang.github.io/) is rebuilt on the fly with GitHub Pages workflow.
+
+
+## Others
+
+* [https://vimalkvn.com/pandoc-markdown-to-html/](https://vimalkvn.com/pandoc-markdown-to-html/)
--- scripts/docs.sh
+++ scripts/docs.sh
@@ -10,18 +10,19 @@ export PYTHONUNBUFFERED=1
 export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
 export TS=$(date +%Y%m%d%H%M%S)
 export DATE=`date "+%Y%m%d"`
-export DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
+export DATE_WITH_TIME=`date "+%Y-%m-%d %H:%M:%S"` #add %3N as we want millisecond too
 
 # functions
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
-cd $baseDir/../docs
+cd $baseDir/..
 
 if [ -f index.html ]; then
     rm -rf index.html
 fi
 
-pandoc --template ../assets/github.html5 \
+pandoc --template ./assets/github.html5 \
         --metadata pagetitle="Hai Liang W's Sharings on GitHub" \
+        --metadata date-meta="$DATE_WITH_TIME" \
         -i README.md -o index.html
