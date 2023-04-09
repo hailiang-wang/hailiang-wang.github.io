4b48d7cc8ce6ec15889b5d7f23c7212dc0d8c087
Hai Liang Wang <hai@chatopera.com>
2023-04-09 12:15:20 +0800

    Update files

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -24,6 +24,7 @@ fi
 
 pandoc --template ./assets/github.html5 \
         --metadata pagetitle="Thoughts and Learnings from Hai Liang W. for Everyone" \
-        --metadata description="Publications of Hai Liang W., subscribe for updates, welcome further contacts or feedbacks." \
+        --metadata description="心理学, 哲学, 科技, 财务, 沟通, 写作, 思考" \
+        --metadata keywords="心理学, 哲学, 科技, 财务, 沟通, 写作, 思考" \
         --metadata date-meta="$DATE_WITH_TIME" \
         -i INDEX.md -o index.html
